2ce6c7625d58ea3087670623efc5a97b71f4033c
Kenza <kinknzakadiri001@gmail.com>
2023-09-24 21:24:36 +0200

    graphique repartition par Genre

--- src/Menu.js
+++ src/Menu.js
@@ -3,23 +3,25 @@ import GenreChart from './GenreChart';
 import { Navbar, Nav } from 'react-bootstrap';
 
 function Menu() {
-    const [showGenreChart, setShowGenreChart] = useState(false);
+  const [showGenreChart, setShowGenreChart] = useState(false);
 
   const handleGenreClick = () => {
-    console.log('Genre Clicked');
     setShowGenreChart(true);
   };
-    return (
-        <nav className="navbar navbar-light bg-light">
-          <div className="btn-group" role="group" aria-label="Menu">
-            <a className="btn btn-outline-success" href="#home">Home</a>
-            <span className="mx-2"></span> {/* Ajoute de l'espace entre les boutons */}
-            <a className="btn btn-outline-secondary" href="#genre">Genre</a>
-            <span className="mx-2"></span> {/* Ajoute de l'espace entre les boutons */}
-            <a className="btn btn-outline-secondary" href="#style">Style</a>
-          </div>
-        </nav>
-      );
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="btn-group" role="group" aria-label="Menu">
+        <a className="btn btn-outline-success" href="#home">Home</a>
+        <span className="mx-2"></span> {/* Ajoute de l'espace entre les boutons */}
+        <a className="btn btn-outline-secondary" href="#genre" onClick={handleGenreClick}>
+          Genre
+        </a>
+        <span className="mx-2"></span> {/* Ajoute de l'espace entre les boutons */}
+        <a className="btn btn-outline-secondary" href="#style">Style</a>
+      </div>
+    </nav>
+  );
 }
 
 export default Menu;
