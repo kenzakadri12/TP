85b966a97d4d94fd3a632714d902ede671a2f48e
Kenza <kinknzakadiri001@gmail.com>
2023-09-25 10:36:08 +0200

    AgechartStyle

--- src/AgeChart.js
+++ src/AgeChart.js
@@ -63,8 +63,8 @@ function AgeChart() {
         {
           label: 'Âge',
           data: participantCounts,
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)', // Couleur d'arrière-plan
+          borderColor: 'rgba(255, 99, 132, 1)', 
           borderWidth: 1,
         },
       ],
